a246e2586b9d10a53da2db95dfab0812954171b2
0sec (On Security) <99223413+0nsec@users.noreply.github.com>
2024-09-28 22:11:26 +0500

    WINDOWS BAT FILE

---
+++ Win.bat
@@ -0,0 +1,26 @@
+@echo off
+
+title Hater Collector by 0nsec
+echo  ______  __      _____                                       
+echo  ___  / / /_____ __  /_____________                          
+echo  __  /_/ /_  __ `/  __/  _ \_  ___/                          
+echo  _  __  / / /_/ // /_ /  __/  /                              
+echo  /_/ /_/  \__,_/ \__/ \___//_/                               
+echo.                                                            
+echo  _________     ___________          _____                    
+echo  __  ____/________  /__  /____________  /_____________       
+echo  _  /    _  __ \_  /__  /_  _ \  ___/  __/  _ \_  ___/       
+echo  / /___  / /_/ /  / _  / /  __/ /__ / /_ /  __/  /           
+echo  \____/  \____//_/  /_/  \___/\___/ \__/ \___//_/            
+echo.
+echo                 by 0nsec v1.0
+
+echo [+] Installing Requirements Pakages.....
+pip install -r requirements.txt
+echo.
+echo [+] Installing Complete.
+echo.
+echo [+] Runing Builder.......
+python builder.py
+
+
